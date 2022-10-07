105dda1cecd585aed7bd8eb88ca449856726fec5
Yurii Tsudz <yura9989@gmail.com>
2022-10-08 00:49:21 +0300

    rmove controller'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AccountModule } from './account/account.module';
-import { AppController } from './app.controller';
 import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
@@ -12,7 +11,7 @@ import { TransactionModule } from './transaction/transaction.module';
       `mongodb://root:pass12345@mongodb:27017/shop?serverSelectionTimeoutMS=2000&authSource=admin`,
     ),
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
